dde41431a6cab3021eac8d601fa1148e8993e92a
andrearigo-dev <andre.rg96@gmail.com>
2022-01-28 10:52:50 +0100

    Added ingradients to data

--- actions/data.py
+++ actions/data.py
@@ -1,7 +1,40 @@
 recipes = [
-            {'name': 'Carbonara'},
-            {'name': 'Amatriciana'},
-            {'name': 'Cacio e pepe'},
-            {'name': 'Muffins'},
-            {'name': 'Waffles'}
+            {'name': 'Carbonara', 'ingridients': [
+                {'name': 'egg yolks', 'amount': 2, 'unit': None},
+                {'name': 'cheek lard', 'amount': 45, 'unit': 'grams'},
+                {'name': 'italian sheep\'s milk cheese', 'amount': 40, 'unit': 'grams'},
+                {'name': 'spaghetti', 'amount': 100, 'unit': 'grams'}
+            ]},
+            {'name': 'Amatriciana', 'ingridients': [
+                {'name': 'tomato sauce', 'amount': 100, 'unit': 'grams'},
+                {'name': 'cheek lard', 'amount': 45, 'unit': 'grams'},
+                {'name': 'italian sheep\'s milk cheese', 'amount': 20, 'unit': 'grams'},
+                {'name': 'spaghetti', 'amount': 100, 'unit': 'grams'}
+            ]},
+            {'name': 'Cacio e pepe', 'ingridients': [
+                {'name': 'italian sheep\'s milk cheese', 'amount': 60, 'unit': 'grams'},
+                {'name': 'spaghetti', 'amount': 100, 'unit': 'grams'},
+                {'name': 'pepper', 'amount': None, 'unit': None}
+            ]},
+            {'name': 'Muffins', 'ingridients': [
+                {'name': 'butter', 'amount': 60, 'unit': 'grams'},
+                {'name': 'sugar', 'amount': 66, 'unit': 'grams'},
+                {'name': 'eggs', 'amount': 1, 'unit': None},
+                {'name': 'pepper', 'amount': None, 'unit': None},
+                {'name': '00 flour', 'amount': '132', 'unit': 'grams'},
+                {'name': 'milk', 'amount': 68, 'unit': 'grams'},
+                {'name': 'dark chocolate chips', 'amount': 50, 'unit': 'grams'},
+                {'name': 'baking powder', 'amount': 5, 'unit': 'grams'},
+                {'name': 'bicarbonate', 'amount': 0.5, 'unit': 'teaspoon'},
+                {'name': 'salt', 'amount': None, 'unit': None}
+            ]},
+            {'name': 'Waffles', 'ingridients': [
+                {'name': '00 flour', 'amount': 140, 'unit': 'grams'},
+                {'name': 'eggs', 'amount': 3, 'unit': None},
+                {'name': 'sugar', 'amount': 140, 'unit': 'grams'},
+                {'name': 'salt', 'amount': 1, 'unit': 'teaspoon'},
+                {'name': 'butter', 'amount': 110, 'unit': 'grams'},
+                {'name': 'vanilla pods', 'amount': 1, 'unit': None},
+                {'name': 'baking powder', 'amount': 1, 'unit': 'grams'}
+            ]}
         ]
